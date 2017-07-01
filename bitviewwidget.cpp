678291da9bc443eca5ff876427e78ee788472f33
jrojer <cibid@yandex.ru>
2017-07-01 21:34:39 +0300

    fixed bug with the partial rendering of a last row

--- bitviewwidget.cpp
+++ bitviewwidget.cpp
@@ -33,6 +33,9 @@ void BitViewWidget::paintEvent(QPaintEvent* event)
 
 	SetScrollBars();
 
+	column_offset_ = hor_scrollbar_->value();
+	row_offset_ = ver_scrollbar_->value();
+
 	QPainter painter(this);
 
 	if(data_.empty())
@@ -46,13 +49,9 @@ void BitViewWidget::paintEvent(QPaintEvent* event)
 
 		painter.scale(grain_size_pixels_,grain_size_pixels_);
 
-		/*
-		 * TODO: handle the last row, it can be partial
-		 */
+		painter.fillRect(0,0,num_cols,num_rows-1,Qt::black);
 
-		painter.fillRect(0,0,num_cols,num_rows,Qt::black);
-
-		for (size_t i = 0; i < num_rows; ++i)
+		for (size_t i = 0; i < num_rows-1; ++i) // all rows except the last one
 		{
 			for (size_t j = 0; j < num_cols; ++j)
 			{
@@ -62,24 +61,47 @@ void BitViewWidget::paintEvent(QPaintEvent* event)
 				}
 			}
 		}
+
+		// taking care of the last row exclusively
+		size_t i = num_rows-1;
+		for (size_t j = 0; j < num_cols; ++j)
+		{
+			size_t bitpos = column_offset_ + j + period_bits_*(row_offset_ + i);
+			if (bitpos >= data_.size()*CHAR_BIT)
+			{
+				return;
+			}
+			if ( GetBit( (const uint8_t*)data_.data(),bitpos) == 1)
+			{
+				// one
+				painter.fillRect(j,i,1,1,Qt::green);
+			}
+			else
+			{
+				// zero
+				painter.fillRect(j,i,1,1,Qt::black);
+			}
+		}
 	}
 
 }
 
 void BitViewWidget::setHorizontalScrollBarValue(int value)
 {
-	column_offset_ = value;
+	//column_offset_ = value;
 	update();
 }
 void BitViewWidget::setVerticalScrollBarValue(int value)
 {
-	row_offset_ = value;
+	//row_offset_ = value;
 	update();
 }
 void BitViewWidget::setPeriod(int value)
 {
-	row_offset_ = 0;
-	column_offset_ = 0;
+	//row_offset_ = 0;
+	//column_offset_ = 0;
+	hor_scrollbar_->setValue(0);
+	ver_scrollbar_->setValue(0);
 	period_bits_ = value;
 	update();
 }
@@ -119,6 +141,8 @@ void BitViewWidget::SetScrollBars()
 	else
 	{
 		hor_scrollbar_->setValue(0);
+		hor_scrollbar_->setMinimum(0);
+		hor_scrollbar_->setMaximum(0);
 		hor_scrollbar_->setDisabled(true);
 	}
 
@@ -132,6 +156,8 @@ void BitViewWidget::SetScrollBars()
 	else
 	{
 		ver_scrollbar_->setValue(0);
+		ver_scrollbar_->setMinimum(0);
+		ver_scrollbar_->setMaximum(0);
 		ver_scrollbar_->setDisabled(true);
 	}
 }
